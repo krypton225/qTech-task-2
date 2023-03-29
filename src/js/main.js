1489fd95c666c5debfc01ee7cae161ac90e45563
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-29 14:18:02 +0200

    ADD project turkey layout with responsive version

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import HeaderSwiper from "./HeaderSwiper.js";
 import SecondSwiper from "./SecondSwiper.js";
+import ThirdSwiper from "./ThirdSwiper.js";
 import Toggler from "./Toggler.js";
 import Scroll from "./Scroll.js";
 
 HeaderSwiper.on();
 SecondSwiper.on();
+ThirdSwiper.on();
 
 Toggler.click("navbar-toggler", "#navbar-toggler span");
 
