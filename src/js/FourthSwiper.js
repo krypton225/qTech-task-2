a17646d9906ac9ba728f082045457dc16106c835
krypton225 <k.mohamed.11.98@gmail.com>
2023-03-29 16:16:08 +0200

    ADD js file for fourth swiper

---
+++ src/js/FourthSwiper.js
@@ -0,0 +1,64 @@
+import "../../node_modules/swiper/swiper-bundle.js";
+
+const FourthSwiper = (function () {
+    function fire() {
+        new Swiper(".dubai-swiper", {
+            spaceBetween: 30,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            grabCursor: true,
+            loop: true,
+
+            // Enabled autoplay mode
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false
+            },
+
+            // If we need pagination
+            pagination: {
+                el: ".dubai-swiper-pagination",
+                dynamicBullets: true,
+                clickable: true
+            },
+
+            // If we need navigation
+            navigation: {
+                nextEl: ".swiper-button-next-4",
+                prevEl: ".swiper-button-prev-4"
+            },
+
+            // Responsive breakpoints
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                480: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                },
+                // 768: {
+                //     slidesPerView: 3,
+                //     spaceBetween: 40,
+                // },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 20
+                }
+            }
+        });
+    }
+
+    return {
+        on: fire
+    };
+})();
+
+export default FourthSwiper;
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import HeaderSwiper from "./HeaderSwiper.js";
 import SecondSwiper from "./SecondSwiper.js";
 import ThirdSwiper from "./ThirdSwiper.js";
+import FourthSwiper from "./FourthSwiper.js";
 import Toggler from "./Toggler.js";
 import Scroll from "./Scroll.js";
 
 HeaderSwiper.on();
 SecondSwiper.on();
 ThirdSwiper.on();
+FourthSwiper.on();
 
 Toggler.click("navbar-toggler", "#navbar-toggler span");
 
